24887702db014fe5edadefabc186cd4d8af10e5e
Guy <guydovlev@gmail.com>
2023-06-23 17:36:24 +0300

    solved 02 03 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+  let result = array.filter((element) => {
+    if(!num.includes(element)) return element;
+  });
 
+  return result;
 };
 
 // Do not edit below this line
