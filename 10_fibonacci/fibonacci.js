f440b15dd0e44117edff633469e7bf25727c36af
Guy <guydovlev@gmail.com>
2023-06-23 20:31:33 +0300

    solved 10:

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+  if(count < 0) return "OOPS";
+  let fibArr = [0,1];
+  for(let i=1; i<count; i++){
+    fibArr.push(fibArr[i] + fibArr[i-1]);
+  }
+  return fibArr[count];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
