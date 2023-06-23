6ee080d34729ebdb500da3e075577fe22eec3cdc
Guy <guydovlev@gmail.com>
2023-06-23 20:26:54 +0300

    solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+// helper function
+const reverseString = function (text) {
+  let result = "";
+  for(let i=text.length-1; i>=0; i--){
+    result += text[i];
+  }
+  return result;
+}
+const palindromes = function (text) {
+  let punctuation = /[^a-zA-Z0-9]+/g;
+  text = text.toLowerCase();
+  text = text.replaceAll(punctuation, '');
+  let reversedText = reverseString(text);
+  console.log(reversedText);
+  if(reversedText == text) return true;
+  else return false;
 };
 
 // Do not edit below this line
